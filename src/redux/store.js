e9eb2cc123fbb64b5a73533fb3f452bcb24fb49c
febe89 <fullstack9020@gmail.com>
2024-05-24 18:34:32 +0530

    empty

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers }from 'redux' 
-import todoReducer from './reducers/todo-reducer'
-const rootReducer = combineReducers({ todos:todoReducer })
-
-const store = createStore(rootReducer)
-
-export default store
